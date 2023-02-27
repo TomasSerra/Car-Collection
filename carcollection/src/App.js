90d7635a309ee1fd7a0c736f8517c0b8316cf5ef
TomasSerra <tomi.serra@gmail.com>
2023-02-27 19:14:49 -0300

    Home page (Cards, Titles and header)

---
+++ carcollection/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './App.scss';
+
+import Home from './pages/home/Home';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
---
+++ carcollection/src/pages/home/Home.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import './Home.scss';
+import { GrFormAdd, GrMenu } from 'react-icons/gr';
+import { IconContext } from "react-icons";
+import nissan from "../../img/nissan1.jpeg"
+
+export default function Component(){
+
+    return(
+        <div className="home-container">
+            <header>
+                <GrMenu size="2rem"/>
+                <h2>Car Collection</h2>
+                <GrFormAdd size="2.5rem"/>
+            </header>
+            <section>
+                <h3 className="title">Nissan</h3>
+                <hr/>
+
+                <div className="cards-container">
+                    <div className="card">
+                        <center>
+                            <div className="image"></div>
+                            <h2>'17 Nissan GT-R (R35)</h2>
+                            <p>HW J-Imports</p>
+                        </center>
+                    </div>
+                    <div className="card">
+                        <center>
+                            <div className="image"></div>
+                            <h2>'17 Nissan GT-R (R35)</h2>
+                            <p>HW J-Imports</p>
+                        </center>
+                    </div>
+                    <div className="card">
+                        <center>
+                            <div className="image"></div>
+                            <h2>'17 Nissan GT-R (R35)</h2>
+                            <p>HW J-Imports</p>
+                        </center>
+                    </div>
+                    <div className="card">
+                        <center>
+                            <div className="image"></div>
+                            <h2>'17 Nissan GT-R (R35)</h2>
+                            <p>HW J-Imports</p>
+                        </center>
+                    </div>
+                    <div className="card">
+                        <center>
+                            <div className="image"></div>
+                            <h2>'17 Nissan GT-R (R35)</h2>
+                            <p>HW J-Imports</p>
+                        </center>
+                    </div>
+                    <div className="card">
+                        <center>
+                            <div className="image"></div>
+                            <h2>'17 Nissan GT-R (R35)</h2>
+                            <p>HW J-Imports</p>
+                        </center>
+                    </div>
+                    <div className="card">
+                        <center>
+                            <div className="image"></div>
+                            <h2>'17 Nissan GT-R (R35)</h2>
+                            <p>HW J-Imports</p>
+                        </center>
+                    </div>
+                    <div className="card">
+                        <center>
+                            <div className="image"></div>
+                            <h2>'17 Nissan GT-R (R35)</h2>
+                            <p>HW J-Imports</p>
+                        </center>
+                    </div>
+                    <div className="card">
+                        <center>
+                            <div className="image"></div>
+                            <h2>'17 Nissan GT-R (R35)</h2>
+                            <p>HW J-Imports</p>
+                        </center>
+                    </div>
+                    <div className="card">
+                        <center>
+                            <div className="image"></div>
+                            <h2>'17 Nissan GT-R (R35)</h2>
+                            <p>HW J-Imports</p>
+                        </center>
+                    </div>
+                    <div className="card">
+                        <center>
+                            <div className="image"></div>
+                            <h2>'17 Nissan GT-R (R35)</h2>
+                            <p>HW J-Imports</p>
+                        </center>
+                    </div>
+                    <div className="card">
+                        <center>
+                            <div className="image"></div>
+                            <h2>'17 Nissan GT-R (R35)</h2>
+                            <p>HW J-Imports</p>
+                        </center>
+                    </div>
+                </div>
+                
+            </section>
+        </div>
+    )
+}
