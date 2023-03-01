5a97c12136a1014a2258e92989983839c66ce5f1
TomasSerra <tomi.serra@gmail.com>
2023-03-01 17:14:05 -0300

    Update image

--- carcollection/src/pages/add/Add.jsx
+++ carcollection/src/pages/add/Add.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import './Add.scss';
 
-export default function Component(){
-    const back = "< Back"
+export default function Add(props){
+
+    const [uploadImage, setUploadImage] = useState();
+
+    function back(){
+        props.handlePage(2)
+    }
+
+    function readImage(){
+        const imageInput = document.getElementById("imageInput")
+        const image = document.getElementById("image")
+        const uploadBtn = document.getElementById("uploadBtn")
+
+        if(imageInput.files && imageInput.files[0]){
+            var reader = new FileReader();
+            reader.onload = function (e){
+                setUploadImage(e.target.result)
+                image.style = "background-image: url("+ e.target.result + "); background-size: auto 100%; background-position-y: 0;"
+                uploadBtn.style = "opacity: 50%;"
+            };
+            reader.readAsDataURL(imageInput.files[0]);
+        }
+    }
 
     return(
         <div className="add-container">
-            <button className="back-button">{back}</button>
+            <button className="back-button" onClick={back}>{"< Back"}</button>
             <input id={"title"} placeholder={"Title"}/>
             <div className="image-container">
-                <div className="image">
-                    <div className="upload-btn">Upload image<input type={"file"} accept={"image/png, image/jpeg"} multiple={false}/></div>
+                <div className="image" id="image">
+                    <div className="upload-btn" id="uploadBtn">Upload image<input type={"file"} accept={"image/png, image/jpeg"} multiple={false} onChange={readImage} id="imageInput"/></div>
                 </div>
             </div>
             <div className="inputs-container">
