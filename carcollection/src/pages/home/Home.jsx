06b0ed2341987929c7fb8f58e58974479bd3b422
TomasSerra <tomi.serra@gmail.com>
2023-03-07 21:46:07 -0300

    Total cars and edit

--- carcollection/src/pages/home/Home.jsx
+++ carcollection/src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ import { GrFormAdd} from 'react-icons/gr';
 import { auth, db, storage } from "../../firebase"
 import { signOut } from "firebase/auth";
 import Card from '../../components/Card';
-import { getDatabase, ref, remove, onValue } from "firebase/database";
+import { getDatabase, ref, remove, onValue, set } from "firebase/database";
 import { ref as refST, deleteObject} from "firebase/storage";
 
 import Add from '../add/Add';
@@ -12,10 +12,12 @@ import Add from '../add/Add';
 export default function Home(props){
     const [warningOpen, setWarningOpen] = useState(false);
     const [deleteOpen, setDeleteOpen] = useState(false);
+    const [infoEdit, setInfoEdit] = useState(false);
     const [data, setData] = useState({});
     const [infoOpen, setInfoOpen] = useState(false)
     const [addOpen, setAddOpen] = useState(false)
     const [username, setUsername] = useState(props.user.displayName)
+    const [totalCars, setTotalCars] = useState(0)
 
     const [titleInfo, setTitle] = useState("")
     const [brandInfo, setBrand] = useState("")
@@ -28,14 +30,33 @@ export default function Home(props){
     const [ownerInfo, setOwner] = useState("")
     const [imageInfo, setImage] = useState("")
 
+    const [collectionNumberNew, setCollectionNumberNew] = useState("")
+    const [seriesNumberNew, setSeriesNumberNew] = useState("")
+    const [yearNew, setYearNew] = useState("")
+    const [dateNew, setDateNew] = useState("")
+    const [ownerNew, setOwnerNew] = useState("")
+
     useEffect(() => {
         const db = getDatabase();
         const starCountRef = ref(db, 'users/'+props.user.uid+'/cars');
         setUsername(props.user.displayName)
 
+        
+
         onValue(starCountRef, (snapshot) => {
-            setData(snapshot.val())
-          });
+            var datos = snapshot.val()
+            setData(datos)
+
+            var total = 0
+        
+            Object.keys(datos ? datos : []).map((brand)=>{
+                Object.values(datos[brand] ? datos[brand] : []).map((car)=>{
+                    total += 1
+                })
+            })
+            setTotalCars(total)
+            
+        });
 
       },[])
 
@@ -71,6 +92,7 @@ export default function Home(props){
 
     function backInfo(){
         setInfoOpen(!infoOpen)
+        setInfoEdit(false)
     }
 
     function deleteCar()
@@ -79,6 +101,46 @@ export default function Home(props){
         deleteObject(refST(storage, 'users/' + props.user.uid + "/cars/"+ brandInfo + "/" +titleInfo))
         setDeleteOpen(false)
         backInfo()
+    }
+
+    function editButton()
+    {
+        const save = document.getElementById("saveEdit")
+
+        if(infoEdit){ //Save
+            if(navigator.onLine){
+                save.disabled = true
+                set(ref(db, 'users/' + props.user.uid + '/cars/' + brandInfo + '/' + titleInfo), {
+                    title: titleInfo,
+                    brand: brandInfo,
+                    collection: collectionInfo,
+                    collectionColor: collectionColorInfo,
+                    collectionNumber: collectionNumberNew,
+                    year: yearNew,
+                    seriesNumber: seriesNumberNew,
+                    date: dateNew,
+                    owner: ownerNew,
+                    image: titleInfo
+                })
+                .then(()=>{
+                    save.disabled = false
+                    setInfoEdit(!infoEdit)
+                })
+                .catch((error)=>{
+                    save.disabled = false
+                })            
+            }
+        }
+        else{ //Edit
+            setInfoEdit(!infoEdit)
+            setCollectionNumberNew(collectionNumberInfo)
+            setSeriesNumberNew(seriesNumberInfo)
+            setDateNew(dateInfo)
+            setYearNew(yearInfo)
+            setOwnerNew(ownerInfo)
+        }
+
+        
     }
 
     return(
@@ -101,7 +163,9 @@ export default function Home(props){
                     <GrFormAdd size="3rem"/>
                 </button>
             </header>
+            <h2 className="totalCars">Total cars: {totalCars}</h2>
             <section className="container">
+            
                 {
                     Object.keys(data ? data : []).map((brand)=>{
                         return(
@@ -112,7 +176,6 @@ export default function Home(props){
                                 <div className="cards-container">
                                     {
                                         Object.values(data[brand] ? data[brand] : []).map((car)=>{
-                                            
                                             return(
                                                 < Card key={props.user.uid+car.title} infoOpen={infoOpenButton} userId={props.user.uid} title={car.title} collection={car.collection} collectionColor={car.collectionColor} collectionNumber={car.collectionNumber} seriesNumber={car.seriesNumber} owner={car.owner} year={car.year} date={car.date}  color={car.collectionColor} image={car.title} brand={car.brand}/>
                                             )
@@ -137,7 +200,10 @@ export default function Home(props){
                 </div>
             </div>
         <div className="info-container" style={{display: infoOpen ? 'block' : 'none'}}>
-            <button className="back-button" onClick={backInfo}>{"< Back"}</button>
+            <div className="buttons-container">
+                <button className="back-button" onClick={backInfo}>{"< Back"}</button>
+                <button className="edit-button" id="saveEdit" onClick={editButton}>{ infoEdit ? "Save" : "Edit"}</button>
+            </div>
             <center>
                 <div className="image-title-container">
                     <div className="image-container">
@@ -150,27 +216,27 @@ export default function Home(props){
                 <div className="details-container">
                     <div className="input-container">
                         <label>Brand</label>
-                        <input disabled placeholder={brandInfo}/>
+                        <input disabled defaultValue={brandInfo}/>
                     </div>
                     <div className="input-container">
                         <label>Year</label>
-                        <input disabled placeholder={yearInfo}/>
+                        <input disabled={!infoEdit} defaultValue={yearInfo} type="number" onChange={(e)=>{setYearNew(e.target.value)}}/>
                     </div>
                     <div className="input-container">
                         <label>Collection number</label>
-                        <input disabled placeholder={collectionNumberInfo}/>
+                        <input disabled={!infoEdit} defaultValue={collectionNumberInfo} onChange={(e)=>{setCollectionNumberNew(e.target.value)}}/>
                     </div>
                     <div className="input-container">
                         <label>Series number</label>
-                        <input disabled placeholder={seriesNumberInfo}/>
+                        <input disabled={!infoEdit} defaultValue={seriesNumberInfo} onChange={(e)=>{setSeriesNumberNew(e.target.value)}}/>
                     </div>
                     <div className="input-container">
                         <label>Date</label>
-                        <input disabled placeholder={dateInfo}/>
+                        <input disabled={!infoEdit} defaultValue={dateInfo} onChange={(e)=>{setDateNew(e.target.value)}} placeholder={infoEdit ? "yyyy-mm-dd" : ""}/>
                     </div>
                     <div className="input-container">
                         <label>Owner</label>
-                        <input disabled placeholder={ownerInfo}/>
+                        <input disabled={!infoEdit} defaultValue={ownerInfo} onChange={(e)=>{setOwnerNew(e.target.value)}}/>
                     </div>
                 </div>
                 <button className="delete-btn" onClick={()=>{setDeleteOpen(true)}}>Delete Car</button>
